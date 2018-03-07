44ac32753316d25a5ee5102931706c7e8bc16c84
MuhammadSaadat\Muhammad Saadat <muhammadsaadat42@gmail.com>
2018-03-07 18:45:15 +0500

    Initial Test
    First test of reading data without threading.

---
+++ ReadDataFromFile/ReadDataFromFile/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadDataFromFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader text = new StreamReader(@"D:\all semesters\8th semester\Compiler Construction\Assignments\ReadDataFromFileUsingThreading\Test File.txt");
+            Console.WriteLine(text.ToString());
+        }
+    }
+}
